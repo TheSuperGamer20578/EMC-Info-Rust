95ab794681bc34bf5f2c3d2093a678c3f282f1a3
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2022-04-23 13:53:20 +1000

    Added Position, Bound and util::map_link and fixed Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+//! EarthMC is a large Minecraft server this package lets you get info about things on that server.
+#![warn(clippy::pedantic)]
+
+use std::fmt::{Display, Formatter};
+
+pub mod util;
+
+#[derive(Debug)]
+pub struct Position {
+    pub x: i32,
+    pub y: i16,
+    pub z: i32,
+}
+
+impl Display for Position {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}/{}/{}", self.x, self.y, self.z)
     }
+}
+
+#[derive(Debug)]
+pub struct Bounds {
+    pub x1: i32,
+    pub z1: i32,
+    pub x2: i32,
+    pub z2: i32,
 }
---
+++ src/util.rs
@@ -0,0 +1,17 @@
+use crate::Position;
+
+/// Return a link to the map at the specified position
+pub fn map_link(position: Position, zoom: u8) -> String {
+    format!("https://earthmc.net/map/?zoom={}&x={}&z={}", zoom, position.x, position.z)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Position;
+    use crate::util::map_link;
+
+    #[test]
+    fn test_map_link() {
+        assert_eq!(map_link(Position { x: 32943, y: 64, z: -13297 }, 6), "https://earthmc.net/map/?zoom=6&x=32943&z=-13297")
+    }
+}
