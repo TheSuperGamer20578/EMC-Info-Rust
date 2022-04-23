e2a11be2cca0807590a9d5f6396663f953196e57
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2022-04-23 15:11:54 +1000

    Added Error

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,37 @@
 //! EarthMC is a large Minecraft server this package lets you get info about things on that server.
 #![warn(clippy::pedantic)]
 
+use std::error;
 use std::fmt::{Display, Formatter};
 
 pub mod util;
+
+pub type Result<T> = core::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    TownNotFound,
+    NationNotFound,
+    ReqwestError(reqwest::Error),
+}
+
+impl error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::TownNotFound => write!(f, "The specified town could not be found"),
+            Error::NationNotFound => write!(f, "The specified nation could not be found"),
+            Error::ReqwestError(error) => write!(f, "A reqwest error has occurred: {}", error),
+        }
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(error: reqwest::Error) -> Self {
+        self::ReqwestError(error)
+    }
+}
 
 #[derive(Debug)]
 pub struct Position {
