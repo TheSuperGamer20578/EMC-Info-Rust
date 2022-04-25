70880fbef302b4edd77373b4de6b0a4be0db8e9e
TheSuperGamer20578 <30369708+TheSuperGamer20578@users.noreply.github.com>
2022-04-25 15:25:59 +1000

    Fixed typo

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,7 @@
 use crate::Position;
 
 /// Return a link to the map at the specified position
-pub fn map_link(position: &Position, &zoom: u8) -> String {
+pub fn map_link(position: &Position, zoom: &u8) -> String {
     format!("https://earthmc.net/map/?zoom={}&x={}&z={}", zoom, position.x, position.z)
 }
 
@@ -12,6 +12,6 @@ mod tests {
 
     #[test]
     fn test_map_link() {
-        assert_eq!(map_link(&Position { x: 32943, y: 64, z: -13297 }, 6), "https://earthmc.net/map/?zoom=6&x=32943&z=-13297")
+        assert_eq!(map_link(&Position { x: 32943, y: 64, z: -13297 }, &6), "https://earthmc.net/map/?zoom=6&x=32943&z=-13297")
     }
 }
